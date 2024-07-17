d44c89584772e8169fe1c419978f362ba9432db8
dalramjwi <dalramjwi@gmail.com>
2024-07-17 12:36:20 +0900

    기본적인 text, textColor, backColor 매개변수로 받고 return 확인

--- src/divClass.tsx
+++ src/divClass.tsx
@@ -1,6 +1,6 @@
 type s = string;
 
-class divClass {
+class DivClass {
   text: s;
   textColor: s;
   backColor: s;
@@ -8,5 +8,12 @@ class divClass {
     this.text = text;
     this.textColor = textColor;
     this.backColor = backColor;
+  }
+  divSet(text: s, textColor: s, backColor: s) {
+    return {
+      text: this.text,
+      color: this.textColor,
+      backgroundColor: this.backColor,
+    };
   }
 }
