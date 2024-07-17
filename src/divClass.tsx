2adbe27110ce398028fef95788f239e926870d7d
dalramjwi <dalramjwi@gmail.com>
2024-07-17 12:48:50 +0900

    jsx 방식으로 변환

--- src/divClass.tsx
+++ src/divClass.tsx
@@ -10,18 +10,9 @@ class DivClass {
     this.backColor = backColor;
   }
   divSet(text: s, textColor: s, backColor: s) {
-    // div 요소 생성
-    const div = document.createElement("div");
-
-    // div의 텍스트 설정
-    div.innerText = text;
-
-    // div의 스타일 설정
-    div.style.color = textColor;
-    div.style.backgroundColor = backColor;
-
-    // 생성된 div 요소 반환
-    return div;
+    return (
+      <div style={{ color: textColor, backgroundColor: backColor }}>{text}</div>
+    );
   }
 }
 export default DivClass;
