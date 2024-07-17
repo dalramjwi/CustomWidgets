dfbc582077e3bab956ebaeee34d935a173bdfbd7
dalramjwi <dalramjwi@gmail.com>
2024-07-17 12:32:39 +0900

    class error exists

---
+++ src/divClass.tsx
@@ -0,0 +1,9 @@
+type s = string;
+
+class divClass {
+  constructor(text: s, textColor: s, backColor: s) {
+    this.text = text;
+    this.textColor = textColor;
+    this.backColor = backColor;
+  }
+}
