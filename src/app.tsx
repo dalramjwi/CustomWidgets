4c246f2b194ec74e5971feea0d6d0392afb0118b
dalramjwi <dalramjwi@gmail.com>
2024-07-17 11:16:48 +0900

    test

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
 import React from "react";
-const App = () => {};
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to my React App!</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
 export default App;
