72d0d504df613f5fbe6d82620e67139678cedc2e
dalramjwi <dalramjwi@gmail.com>
2024-07-17 12:47:51 +0900

    div 사용하려 했으나 error 발생 : HTMLDivElement 반환은 react에서 JSX로 변환이 필요하다

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import DivClass from "./divClass";
 const App = () => {
+  const div1 = new DivClass("Welcome!", "white", "blue");
+
   return (
     <div>
       <h1>Welcome to my React App!</h1>
       <p>This is a simple React application.</p>
+      {div1.divSet()}
     </div>
   );
 };
