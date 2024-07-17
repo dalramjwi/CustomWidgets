5f89e772c5747ae0751109ae3147fbf0c9ab35ff
dalramjwi <dalramjwi@gmail.com>
2024-07-17 12:33:11 +0900

    class error 해결

--- src/divClass.tsx
+++ src/divClass.tsx
@@ -1,6 +1,9 @@
 type s = string;
 
 class divClass {
+  text: s;
+  textColor: s;
+  backColor: s;
   constructor(text: s, textColor: s, backColor: s) {
     this.text = text;
     this.textColor = textColor;
